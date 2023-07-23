bcc3c0f6d02ed2b21ce429a39251f629abd53ad3
MatheusHLM <109868999+Strifexss@users.noreply.github.com>
2023-07-23 18:47:05 +0000

    #

--- src/app/components/notificationElement/index.tsx
+++ src/app/components/notificationElement/index.tsx
@@ -1,32 +1,53 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-import {ReactNode} from "react";
 
 interface Propriedades {
     nome: string;
     provedor: string
-    hasAction: boolean;
-    img: ReactNode;
+    hasAction?: boolean;
+    iconProv: string | StaticImageData;
+    imagemUser: string | StaticImageData;
+    mensagem: string;
 }
 
-export default function index({img, nome, provedor, hasAction = false}:Propriedades) {
+export default function index({imagemUser, iconProv, nome, mensagem, provedor, hasAction = false}:Propriedades) {
     
     return(
-        <div className="cursor-pointer w-full h-auto border-b-2 border-[#242424]">
-            <div className="w-full h-[5rem] p-2 flex flex-col items=center">
+        <div className="cursor-pointer w-full h-auto border-b-2 border-[#242424] hover:bg-[#292929]">
+            <div className="w-full h-[6rem] mb-3 p-2 flex flex-col items=center">
                 <section className="w-full pl-4 flex flex=row items-center gap-2 mb-2">
                     <Image
-                        src={img}
+                        src={iconProv}
                         width={15}
                         height={15}
+                        alt="Icone"
                     /> 
                     <p className="text-[0.7rem] font-bold">{provedor}</p>
                 </section>
-                <section className="w-full flex flex-col justify-center pl-4"> 
+                <div className="w-full flex flex-row items-start mt-2 pl-4">
+                    <Image className="rounded-[50%]"
+                        src={imagemUser}
+                        width={40}
+                        height={40}
+                        alt="Foto"
+                    />
+                <section className="w-full flex flex-col justify-center ml-4"> 
                     <h1 className="font-bold">{nome}</h1>
-                    
+                    <h1 className="text-[0.8rem] text-[#999999]">{mensagem}</h1>
+                    <h1 className="text-[0.8rem] text-[#999999]">Hoje, 10:21am</h1>
                 </section>
+                </div>
             </div>
+            {hasAction &&
+                <div className="h-[3rem] w-full flex flex-row justify-end pr-4 mt-4 gap-2">
+                    <button className="bg-[#e61010] hover:bg-[#941f1f] w-[5rem] h-[2rem] ">
+                        Recusar
+                    </button>
+                    <button className="bg-[#1cc414] hover:bg-[#2d8c29] w-[5rem] h-[2rem]  ">
+                        Aceitar
+                    </button>
+                </div>    
+            }
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
-import Image from 'next/image'
-import NotificationElement from "./components/notificationElement/index.tsx"
+import NotificationElement from "./components/notificationElement/index"
 import InstagramIcon from "./imgs/instagram.png";
+import Matheus from "./imgs/109868999.jpg";
+import { BellDot } from "lucide-react";
+
 export default function Home() {
 
   return (
     <div 
     className='w-screen h-screen flex justify-center items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black'>
-      <div className='w-[25%] h-[70%] bg-[#313131] rounded-xl flex flex-col items-center shadow-2xl'> 
+      <div className='w-[25%] h-[70%] bg-[#313131] rounded-md flex flex-col items-center shadow-2xl'> 
         <div 
-        className="h-[3rem] w-full border-b-2 border-[#242424] p-4  flex flex-row justify-start items-center"> 
+        className="h-[3rem] w-full border-b-2 border-[#242424] p-4  flex flex-row justify-between items-center"> 
           <h1 className="font-bold m-4">Notificações</h1>
+          <BellDot/>
         </div>
         <section className="w-full h-full overflow-y-scroll">
           <NotificationElement
-           provedor="Instagram" nome="Matheus Henrique" img={InstagramIcon}/>
+            hasAction 
+            provedor="Instagram" 
+            nome='Matheus Henrique' 
+            iconProv={InstagramIcon} 
+            imagemUser={Matheus} 
+            mensagem="Nova solicitação pra seguir" 
+          />
+          <NotificationElement
+            provedor="Youtube" 
+            nome='FlowGames' 
+            iconProv={InstagramIcon} 
+            imagemUser={Matheus} 
+            mensagem="Compra da ActvisionBlizzard" 
+          />
         </section>
       </div>
     </div>
