91408afb2b2e8b7aef5ca2d03030ddc5d73625c1
MatheusHLM <109868999+Strifexss@users.noreply.github.com>
2023-07-23 18:54:46 +0000

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div 
     className='w-screen h-screen flex justify-center items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black'>
-      <div className='w-[25%] h-[70%] bg-[#313131] rounded-md flex flex-col items-center shadow-2xl'> 
+      <div className='w-[80%] h-[90%] md:w-[25%] md:h-[70%] bg-[#313131] rounded-md flex flex-col items-center shadow-2xl'> 
         <div 
         className="h-[3rem] w-full border-b-2 border-[#242424] p-4  flex flex-row justify-between items-center"> 
           <h1 className="font-bold m-4">Notificações</h1>
